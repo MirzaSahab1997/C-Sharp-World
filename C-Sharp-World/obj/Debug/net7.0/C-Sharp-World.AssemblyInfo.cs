a8c15edfd0e0929c077a5b490dfba0ed4bbad803
Zeeshan Nadeem <zeeshan.nadeem@caary.com>
2024-03-17 19:14:36 +0500

    Add Gitignore

--- C-Sharp-World/obj/Debug/net7.0/C-Sharp-World.AssemblyInfo.cs
+++ C-Sharp-World/obj/Debug/net7.0/C-Sharp-World.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-Sharp-World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3ab39bebff97407a7747cc4ca7ab88da733c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-Sharp-World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-Sharp-World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
